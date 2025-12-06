b1c00e6ee72399bbf685b45845022ec0898dc80f
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-12-06 16:53:14 -0300

    add @nestjs/config and configure database connection via environment variables

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,21 +1,24 @@
 // src/prisma/prisma.service.ts
-import { Injectable } from '@nestjs/common'
+import { Injectable, OnModuleDestroy } from '@nestjs/common'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { Pool } from 'pg'
 import { PrismaClient } from 'src/generated/prisma/client'
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleDestroy {
+  private pool: Pool
+
   constructor() {
-    const pool = new Pool({
-      user: 'postgres',
-      password: 'docker',
-      host: 'localhost',
-      port: 5432,
-      database: 'nest-clean',
-    })
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL })
 
     const adapter = new PrismaPg(pool)
     super({ adapter })
+
+    this.pool = pool
+  }
+
+  async onModuleDestroy() {
+    await this.pool.end()
+    await this.$disconnect()
   }
 }
