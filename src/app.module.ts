bae39b2acafc8522cd2e220cf6acfece65b88a2e
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-12-07 10:35:45 -0300

    integrate environment schema validation in app module and use typed config service for port configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
+import { envSchema } from './env'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validate: (env) => envSchema.parse(env),
     }),
   ],
   controllers: [CreateAccountController],
