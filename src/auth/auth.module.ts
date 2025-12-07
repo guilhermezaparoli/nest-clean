a72f1ec0ed73acaaf62ec946f09b9ee58ec90461
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-12-07 14:48:06 -0300

    add auth module with jwt configuration using rs256 algorithm and passport integration

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { Env } from 'src/env'
+import { JtwStrategy } from './jtw.strategy'
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      global: true,
+      useFactory(config: ConfigService<Env, true>) {
+        const privateKey = config.get('JWT_PRIVATE_KEY')
+        const publicKey = config.get('JWT_PUBLIC_KEY')
+
+        return {
+          signOptions: {
+            algorithm: 'RS256',
+          },
+          privateKey: Buffer.from(privateKey, 'base64'),
+          publicKey: Buffer.from(publicKey, 'base64'),
+        }
+      },
+    }),
+  ],
+  providers: [JtwStrategy],
+})
+export class AuthModule {}
