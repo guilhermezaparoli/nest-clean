60aeee9b9b227241f232ce4960ebc852c951a5a0
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-12-08 08:11:27 -0300

    add request body validation with zod schema and implement question creation with slug generation in create question controller

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,13 +1,52 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import { CurrentUser } from 'src/auth/current-user.decorator'
+import type { UserPayload } from 'src/auth/jtw.strategy'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import z from 'zod'
 
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+type CreateQuestionType = z.infer<typeof createQuestionBodySchema>
 @Controller('/questions')
+@UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor() {}
+  constructor(private prismaService: PrismaService) {}
 
   @Post()
-  @UseGuards(JwtAuthGuard)
-  async handle() {
-    return 'autenticado'
+  async handle(
+    @Body(new ZodValidationPipe(createQuestionBodySchema))
+    body: CreateQuestionType,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { title, content } = body
+    const authorId = user.sub
+
+    await this.prismaService.question.create({
+      data: {
+        title,
+        content,
+        slug: this.createFromText(title),
+        authorId,
+      },
+    })
+  }
+
+  private createFromText(text: string) {
+    const slugText = text
+      .normalize('NFKD')
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, '-')
+      .replace(/[^\w-]+/g, '')
+      .replace(/_/g, '-')
+      .replace(/--+/g, '-')
+      .replace(/-$/g, '')
+
+    return slugText
   }
 }
