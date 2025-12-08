476b03f794cdeb6f6053a53f4a2c0a95e4a06368
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-12-08 08:11:10 -0300

    rename token schema to user payload and export type for reuse in jwt strategy validation

--- src/auth/jtw.strategy.ts
+++ src/auth/jtw.strategy.ts
@@ -5,11 +5,11 @@ import { ExtractJwt, Strategy } from 'passport-jwt'
 import { Env } from 'src/env'
 import z from 'zod'
 
-const tokenSchema = z.object({
+const tokenPayloadSchema = z.object({
   sub: z.cuid(),
 })
 
-type TokenSchema = z.infer<typeof tokenSchema>
+export type UserPayload = z.infer<typeof tokenPayloadSchema>
 
 @Injectable()
 export class JtwStrategy extends PassportStrategy(Strategy) {
@@ -24,7 +24,7 @@ export class JtwStrategy extends PassportStrategy(Strategy) {
     })
   }
 
-  async validate(payload: TokenSchema) {
-    return tokenSchema.parse(payload)
+  async validate(payload: UserPayload) {
+    return tokenPayloadSchema.parse(payload)
   }
 }
