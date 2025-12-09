b9eb977de66176dd924a28a6064e5355022f7c58
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-12-09 19:40:23 -0300

    add fetch recent questions controller with pagination support and jwt authentication for get endpoint

---
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Query, UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import z from 'zod'
+
+const pageParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+const pageParamSchemaValidation = new ZodValidationPipe(pageParamSchema)
+
+type PageParamsType = z.infer<typeof pageParamSchema>
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class FetchRecentQuestionsController {
+  constructor(private prismaService: PrismaService) {}
+
+  @Get()
+  async handle(@Query('page', pageParamSchemaValidation) page: PageParamsType) {
+    const perPage = 1
+
+    const questions = await this.prismaService.question.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: perPage,
+      skip: (page - 1) * perPage,
+    })
+
+    return {
+      questions,
+    }
+  }
+}
