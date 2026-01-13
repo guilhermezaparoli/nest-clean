2cb9a439e3b6c3ff7ae9a4e2f172ab5c86791911
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2026-01-12 22:20:39 -0300

    add email and password properties to student entity with getter methods for name, email, and password access

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -2,8 +2,21 @@ import { Entity } from '@/core/entities/entity'
 
 interface StudentProps {
   name: string
+  email: string
+  password: string
 }
 export class Student extends Entity<StudentProps> {
+  get name() {
+    return this.name
+  }
+
+  get email() {
+    return this.email
+  }
+
+  get password() {
+    return this.password
+  }
   static create(props: StudentProps) {
     const student = new Student({
       ...props,
