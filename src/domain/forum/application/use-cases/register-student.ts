7746b647f5c1fdb39861b0911115040e1405b44b
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2026-01-12 22:20:23 -0300

    add register student use case with email uniqueness validation and password hashing using hash generator

---
+++ src/domain/forum/application/use-cases/register-student.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common'
+import { HashGenerator } from '../cryptography/hash-generator'
+import { Student } from '../../enterprise/entities/student'
+import { Either, left, right } from '@/core/either'
+import { StudentRepository } from '../repositories/user-repository'
+import { StudentAlreadyExistsError } from './errors/student-already-exists'
+
+interface RegisterStudentUseCaseRequest {
+  name: string
+  password: string
+  email: string
+}
+
+type RegisterStudentUseCaseResponse = Either<
+  StudentAlreadyExistsError,
+  {
+    student: Student
+  }
+>
+
+@Injectable()
+export class RegisterStudentUserUseCase {
+  constructor(
+    private readonly studentRepository: StudentRepository,
+    private hashGenerator: HashGenerator,
+  ) {}
+
+  async exec({
+    email,
+    name,
+    password,
+  }: RegisterStudentUseCaseRequest): Promise<RegisterStudentUseCaseResponse> {
+    const userWithSameEmail = await this.studentRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      return left(new StudentAlreadyExistsError(email))
+    }
+
+    const hashedPassword = await this.hashGenerator.hash(password)
+
+    const student = Student.create({
+      email,
+      name,
+      password: hashedPassword,
+    })
+
+    await this.studentRepository.create(student)
+
+    return right({
+      student,
+    })
+  }
+}
