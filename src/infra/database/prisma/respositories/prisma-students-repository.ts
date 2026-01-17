75f2fac66b7199905c11ab7826b924aea429e52d
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2026-01-17 14:57:21 -0300

    add prisma students repository implementation with find by email and create methods using student mapper for domain and persistence transformations

---
+++ src/infra/database/prisma/respositories/prisma-students-repository.ts
@@ -0,0 +1,30 @@
+import { StudentRepository } from '@/domain/forum/application/repositories/user-repository'
+import { Student } from '@/domain/forum/enterprise/entities/student'
+import { PrismaService } from '../prisma.service'
+import { PrismaStudentMapper } from '../mappers/prisma-student-mapper'
+
+export class PrismaStudentsRepository implements StudentRepository {
+  constructor(private prisma: PrismaService) {}
+  async findByEmail(email: string): Promise<Student | null> {
+    const student = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (!student) {
+      return null
+    }
+
+    return PrismaStudentMapper.toDomain(student)
+  }
+  async create(user: Student): Promise<Student> {
+    const persintanceData = PrismaStudentMapper.toPersistance(user)
+
+    const createdStudent = await this.prisma.user.create({
+      data: persintanceData,
+    })
+
+    return PrismaStudentMapper.toDomain(createdStudent)
+  }
+}
