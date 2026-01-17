939d5ed264d19260d816c67a038ee3bc61e571fe
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2026-01-17 14:56:34 -0300

    add optional id parameter to student factory for entity creation with unique entity id support and update create method call formatting

--- test/factories/make-student.ts
+++ test/factories/make-student.ts
@@ -1,14 +1,22 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import {
   Student,
   StudentProps,
 } from '@/domain/forum/enterprise/entities/student'
 import { faker } from '@faker-js/faker'
 
-export function makeStudent(override?: Partial<StudentProps>): Student {
-  return Student.create({
-    email: faker.internet.email(),
-    name: faker.person.fullName(),
-    password: faker.internet.password(),
-    ...override,
-  })
+export function makeStudent(
+  override?: Partial<StudentProps>,
+  id?: UniqueEntityID,
+): Student {
+  return Student.create(
+    {
+      email: faker.internet.email(),
+      name: faker.person.fullName(),
+      password: faker.internet.password(),
+      role: 'STUDENT',
+      ...override,
+    },
+    id,
+  )
 }
