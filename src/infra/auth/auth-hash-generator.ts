f69a5af98e5f810a6025a65f38a095f0b4c9d4fc
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2026-01-17 14:57:39 -0300

    add prisma student mapper with domain and persistence transformations including unique entity id conversion and student role mapping

--- src/infra/auth/auth-hash-generator.ts
+++
@@ -1,5 +0,0 @@
-import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
-
-// export class AuthHashGenerator implements HashGenerator {
-// async hash(plain: string): Promise<string> {}
-// }
---
+++ src/infra/database/prisma/mappers/prisma-student-mapper.ts
@@ -0,0 +1,29 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Student } from '@/domain/forum/enterprise/entities/student'
+import { User } from 'generated/prisma/client'
+
+export class PrismaStudentMapper {
+  static toDomain(raw: User): Student {
+    const { email, id, name, password, role } = raw
+    return Student.create(
+      {
+        email,
+        name,
+        password,
+        role,
+      },
+      new UniqueEntityID(id),
+    )
+  }
+  static toPersistance(student: Student): User {
+    const { email, id, name, password } = student
+
+    return {
+      id: id.toString(),
+      email,
+      name,
+      password,
+      role: 'STUDENT',
+    }
+  }
+}
