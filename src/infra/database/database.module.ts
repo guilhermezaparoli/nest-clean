90b7dd38e5ee350e9094f2abf7ab6a76d609e606
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2026-01-23 08:48:26 -0300

    add get question by slug controller and use case to http module providers and controllers while fixing prisma answers repository import path from prisma-answer-repository to prisma-answers-repository

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { PrismaQuestionCommentsRepository } from './prisma/respositories/prisma-question-comments-repository'
 import { PrismaQuestionAttachmentsRepository } from './prisma/respositories/prisma-question-atachments-repository'
-import { PrismaAnswerRepository } from './prisma/respositories/prisma-answer-repository'
 import { PrismaAnswerAttachmentsRepository } from './prisma/respositories/prisma-answer-attachments-repository'
 import { PrismaAnswerCommentsRepository } from './prisma/respositories/prisma-answer-comments-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { PrismaQuestionsRepository } from './prisma/respositories/prisma-questions-repository'
 import { StudentRepository } from '@/domain/forum/application/repositories/user-repository'
 import { PrismaStudentsRepository } from './prisma/respositories/prisma-students-repository'
+import { PrismaAnswerRepository } from './prisma/respositories/prisma-answers-repository'
 
 @Module({
   providers: [
